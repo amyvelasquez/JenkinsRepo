dbb36a26b47f2af2df82c7df4bef2f6bff2b1fa2
amyvelasquez <velasquezamy213@gmail.com>
2021-07-21 21:11:57 -0400

    1st commit

---
+++ src/src/com/collections/Main.java
@@ -0,0 +1,11 @@
+package src.com.collections;
+
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        for ( int i = 0; i < 5 ; i++){
+
+            System.out.println("Batch 9 is great");
+            Thread.sleep(1000);
+        }
+    }
+}
